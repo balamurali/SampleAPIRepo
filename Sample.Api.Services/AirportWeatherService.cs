f3643b9396a381593b9ba095acc6b41c3e8b1c20
Balamuraly Unnithan <balamurali.unnithan@gmail.com>
2020-11-01 15:32:31 +0000

    Sample.APi.Services

---
+++ Sample.Api.Services/AirportWeatherService.cs
@@ -0,0 +1,25 @@
+﻿using Sample.API.Repository.Interfaces;
+using Sample.API.Services.Interfaces;
+using Sample.API.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.API.Services
+{
+    public class AirportWeatherService : IAirportWeatherService
+    {
+        private readonly IFlightAwareRepository _repository;
+
+        public AirportWeatherService(IFlightAwareRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        public async Task<AirportWeather> GetAirportWeather(string airportCode)
+        {
+            return await _repository.GetAirportWeather(airportCode);
+        }
+    }
+}
---
+++ Sample.Api.Services/Interfaces/IAirportWeatherService.cs
@@ -0,0 +1,13 @@
+﻿using Sample.API.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.API.Services.Interfaces
+{
+    public interface IAirportWeatherService
+    {
+        Task<AirportWeather> GetAirportWeather(string airportCode);
+    }
+}
